e70e6175ef1f653d32d62039a2ad51c0efc4c988
virajj014 <76771864+virajj014@users.noreply.github.com>
2021-01-08 14:24:48 +0530

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -5,12 +5,6 @@ int main(){
     int y;
     cout<<"enter a no. ";
     cin>> x;
-    cout<<"enter another no. ";
-    cin>> y;
-    cout<<"sum is "<< x+y;
-    cout<<"\ndifference is "<< x-y;
-    cout<<"\nproduct is "<< x*y;
-    cout<<"\ndivision is "<< x/y;
-
+    cout<<"You entered "<< x;
 }
 
