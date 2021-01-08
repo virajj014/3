f045753cbcdd1756d0030a3c6c3cd734d441d765
virajj014 <76771864+virajj014@users.noreply.github.com>
2021-01-08 14:23:36 +0530

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    cout<<"enter a no. ";
+    cin>> x;
+    cout<<"enter another no. ";
+    cin>> y;
+    cout<<"sum is "<< x+y;
+    cout<<"\ndifference is "<< x-y;
+    cout<<"\nproduct is "<< x*y;
+    cout<<"\ndivision is "<< x/y;
+
+}
+
